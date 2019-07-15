aae08db874eaaff7f02653daaf817fc2542c6dae
Leah <musicalshorty65@gmail.com>
2019-07-15 14:29:22 -0600

    finished UI

---
+++ Azoera/CustomUI/AzoeraButton.swift
@@ -0,0 +1,21 @@
+//
+//  AzoeraButton.swift
+//  Azoera
+//
+//  Created by Leah Cluff on 7/15/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class AzoeraButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

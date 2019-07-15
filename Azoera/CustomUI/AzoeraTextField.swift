6319ff0eb29c51ca2569c0be155c6eb43cc74130
Leah <musicalshorty65@gmail.com>
2019-07-15 12:14:10 -0600

    initial commit

---
+++ Azoera/CustomUI/AzoeraTextField.swift
@@ -0,0 +1,21 @@
+//
+//  AzoeraTextField.swift
+//  Azoera
+//
+//  Created by Leah Cluff on 7/15/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class AzoeraTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Azoera/CustomUI/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Azoera
+//
+//  Created by Leah Cluff on 7/15/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
